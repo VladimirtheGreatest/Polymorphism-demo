5c3ab695c40512e22cd345b359008d9dadf99091
Vladimir <vladkodoktorov@gmail.com>
2021-04-04 11:33:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polymorphism_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
